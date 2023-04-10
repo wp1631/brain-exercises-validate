ce34a21ee7c116745a9613cf9232866727d417c0
Thiti Chainiyom <97273062+tokecny@users.noreply.github.com>
2023-04-11 05:21:15 +0700

    revise SSLanding

--- src/pages/gameLanding/ssLanding/SSLanding.tsx
+++ src/pages/gameLanding/ssLanding/SSLanding.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import './SSLanding.css'
+import AppHeader from '../../.././components/appHeader/AppHeader'
+import SSWindow from '../../.././components/gameWindow/ssWindow/SSWindow'
+import EnterButtons from '../../.././components/enterButtons/EnterButtons'
 
 function SSLanding() {
   return (
     <div className='container-fluid'>
         <div className='row'>
-            <div id='header-banner' className='col'></div>
-            <div id='body-window' className='col'></div>
+            <div id='SSLandingHeader' className='col'>
+              {<AppHeader />}
+            </div>
+            <div id='SSLandingBody' className='col'>
+              <div className="SSLandingProgressBar"></div>
+              <div className="SSLandingWindow">
+                {<SSWindow />}
+              </div>
+              <div className="SSLandingEnterButton">
+                {<EnterButtons />}
+              </div>
+            </div>
         </div>
     </div>
   )
