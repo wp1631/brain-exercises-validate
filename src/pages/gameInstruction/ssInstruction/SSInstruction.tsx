2ef8f67fb54e32bf4eaf2fdf3d58bde70fda6a07
Thiti Chainiyom <97273062+tokecny@users.noreply.github.com>
2023-04-11 05:21:37 +0700

    revise SSInstruction

--- src/pages/gameInstruction/ssInstruction/SSInstruction.tsx
+++ src/pages/gameInstruction/ssInstruction/SSInstruction.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import './SSInstruction.css'
+import AppHeader from '../../../components/appHeader/AppHeader';
+import SSWindow from '../../../components/gameWindow/ssWindow/SSWindow';
+import ProgressBar from '../../../components/progressBar/ProgressBar';
 
 function SSInstruction() {
   return (
     <div className='container-fluid'>
         <div className='row'>
-            <div id='header-banner' className='col'></div>
-            <div id='body-window' className='col'></div>
+            <div id='SSInstructionHeader' className='col'>
+              {<AppHeader />}
+            </div>
+            <div id='SSInstructionBody' className='col'>
+            <div className="SSInstructionBodyProgressBar">
+              {/* {<ProgressBar progressValue={progressValue} trialNumber={trialNumber}/>} */}
+              {<ProgressBar />}
+            </div>
+              <div className="SSInstructionWindow">
+                {/* {<SSWindow checkSeq={checkSeq}/>} */}
+                {<SSWindow />}
+              </div>
+              <div className="SSInstructionEnterButton"></div>
+            </div>
         </div>
     </div>
   )
