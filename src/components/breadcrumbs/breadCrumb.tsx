73259295da84c524452b2fbb0041cfb64b6a0921
WP_MOS <63374544+wp1631@users.noreply.github.com>
2023-04-21 17:15:08 +0700

    Create breadCrumb.tsx

---
+++ src/components/breadcrumbs/breadCrumb.tsx
@@ -0,0 +1,44 @@
+import { HomeIcon } from '@heroicons/react/20/solid'
+
+const pages = [
+  { name: 'เกม', href: '/games', current: false },
+  { name: 'วิญญาณตามติด', href: '/games/insidious', current: true },
+]
+
+export default function Example() {
+  return (
+    <nav className="flex h-fit" aria-label="Breadcrumb">
+      <ol role="list" className="flex items-center space-x-4">
+        <li>
+          <div>
+            <a href="/home" className="text-gray-400 hover:text-gray-500 ">
+              <HomeIcon className="h-5 w-5 sm:h-8 sm:w-8 flex-shrink-0" aria-hidden="true" />
+              <span className="sr-only">Home</span>
+            </a>
+          </div>
+        </li>
+        {pages.map((page) => (
+          <li key={page.name}>
+            <div className="flex items-center">
+              <svg
+                className="h-5 w-5 sm:h-8 sm:w-8 flex-shrink-0 text-gray-300"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                aria-hidden="true"
+              >
+                <path d="M5.555 17.776l8-16 .894.448-8 16-.894-.448z" />
+              </svg>
+              <a
+                href={page.href}
+                className="ml-4 text-sm sm:text-lg font-medium sm:font-medium text-gray-500 hover:text-gray-700"
+                aria-current={page.current ? 'page' : undefined}
+              >
+                {page.name}
+              </a>
+            </div>
+          </li>
+        ))}
+      </ol>
+    </nav>
+  )
+}
