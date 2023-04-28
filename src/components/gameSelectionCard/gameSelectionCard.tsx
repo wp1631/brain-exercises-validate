a8ad8fcd8a37335198757d1f31f4041c8c24194d
Thiti Chainiyom <97273062+tokecny@users.noreply.github.com>
2023-04-28 20:01:45 +0700

    remove duplicate IGameInfo

--- src/components/gameSelectionCard/gameSelectionCard.tsx
+++ src/components/gameSelectionCard/gameSelectionCard.tsx
@@ -31,39 +31,6 @@ interface IGameInfo {
     gameUri: string;
     imageUrl: string;
 }
-import { PlayIcon } from '@heroicons/react/20/solid'
-
-const people = [
-  {
-    name: 'Jane Cooper',
-    title: 'Paradigm Representative',
-    role: 'Admin',
-    email: 'janecooper@example.com',
-    telephone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  // More people...
-]
-
-const gameInfos = [
-    {
-        name: 'จำจด กดตาม',
-        title: 'Spatial span',
-        domain: 'ความจำ',
-        gameUri: 'spatial-span',
-        imageUrl:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-    }
-]
-
-interface IGameInfo {
-    name: string;
-    title: string;
-    domain: string;
-    gameUri: string;
-    imageUrl: string;
-}
 
 export default function GameSelectionCards(props: {games: IGameInfo[]}) {
   return (
