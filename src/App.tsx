0c3f190e796f732ddc07bcfbba3681720085ee23
Thiti Chainiyom <97273062+tokecny@users.noreply.github.com>
2023-05-09 22:33:01 +0700

    remove +"#/"

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ function App() {
       setUserPhone(phone);
       // window.location.replace(window.location.origin + "#/landing");
     } else {
-      if (window.location.href === "https://cccnlab.co/brain-exercises-hard/" +"#/"){
+      if (window.location.href === "https://cccnlab.co/brain-exercises-hard/"){
         console.log(window.location.href)
       } else {
         window.location.replace("https://cccnlab.co/brain-exercises-hard/");
