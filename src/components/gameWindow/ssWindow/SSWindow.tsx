13e17a3eb22e7a421d56e671df5967b4ce538eba
Thiti Chainiyom <97273062+tokecny@users.noreply.github.com>
2023-05-24 00:06:40 +0700

    revise cirButton
    - add cirButtonBorder in every buttons
    - add hoverDisabled to prevent clicking in game landing page

--- src/components/gameWindow/ssWindow/SSWindow.tsx
+++ src/components/gameWindow/ssWindow/SSWindow.tsx
@@ -6,12 +6,12 @@ function SSWindow(props) {
   return (
     <div className="cirBtnContainer">
       <div className="signal readyToClick" id="goSignal"></div>
-      <button className="cirButton 1" id='cirButton1' onClick={props.checkSeq}></button>
-      <button className="cirButton 2" id='cirButton2' onClick={props.checkSeq}></button>
-      <button className="cirButton 3" id='cirButton3' onClick={props.checkSeq}></button>
-      <button className="cirButton 4" id='cirButton4' onClick={props.checkSeq}></button>
-      <button className="cirButton 5" id='cirButton5' onClick={props.checkSeq}></button>
-      <button className="cirButton 6" id='cirButton6' onClick={props.checkSeq}></button>            
+      <button className="cirButton 1 hoverDisabled" id='cirButton1' onClick={props.checkSeq}><div className="cirButtonBorder" id='border1'></div></button>
+      <button className="cirButton 2 hoverDisabled" id='cirButton2' onClick={props.checkSeq}><div className="cirButtonBorder" id='border2'></div></button>
+      <button className="cirButton 3 hoverDisabled" id='cirButton3' onClick={props.checkSeq}><div className="cirButtonBorder" id='border3'></div></button>
+      <button className="cirButton 4 hoverDisabled" id='cirButton4' onClick={props.checkSeq}><div className="cirButtonBorder" id='border4'></div></button>
+      <button className="cirButton 5 hoverDisabled" id='cirButton5' onClick={props.checkSeq}><div className="cirButtonBorder" id='border5'></div></button>
+      <button className="cirButton 6 hoverDisabled" id='cirButton6' onClick={props.checkSeq}><div className="cirButtonBorder" id='border6'></div></button>            
     </div>
   )
 }
